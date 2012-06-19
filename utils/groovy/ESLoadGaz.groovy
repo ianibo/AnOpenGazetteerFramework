230a6027a411692e8e34f4e0b39085b59cb36dc9
Ian Ibbotson <ian.ibbotson@k-int.com>
2012-06-19 15:10:47 +0100

    updated dependency version

--- utils/groovy/ESLoadGaz.groovy
+++ utils/groovy/ESLoadGaz.groovy
@@ -4,7 +4,7 @@
 // @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
 @Grapes([
   @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
-  @Grab(group='org.elasticsearch', module='elasticsearch', version='0.19.0.RC2'),
+  @Grab(group='org.elasticsearch', module='elasticsearch', version='0.19.4'),
   @Grab(group='org.elasticsearch', module='elasticsearch-lang-groovy', version='1.1.0')])
 
 import org.apache.log4j.*
