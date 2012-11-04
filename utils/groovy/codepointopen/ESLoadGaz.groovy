efb7b9829d5ce269ea65a955940f28fea229c493
Ian Ibbotson <ianibbo@googlemail.com>
2012-11-04 18:38:41 +0000

    Cleaned up some refdata

--- utils/groovy/codepointopen/ESLoadGaz.groovy
+++ utils/groovy/codepointopen/ESLoadGaz.groovy
@@ -12,6 +12,10 @@
 import org.apache.log4j.*
 import uk.me.jstott.jcoord.*;
 import au.com.bytecode.opencsv.CSVReader
+import org.elasticsearch.groovy.client.GClient
+import org.elasticsearch.groovy.node.GNode
+
+
 
 
 LogManager.rootLogger.level = Level.DEBUG
@@ -31,9 +35,14 @@ else {
   // To clear down the gaz: curl -XDELETE 'http://localhost:9200/gaz'
   setup(esclient);
 
+  def codes = loadCodes();
+
   def charset = java.nio.charset.Charset.forName('UTF-8'); // ISO-8859-1
   CSVReader r = new CSVReader( new InputStreamReader(new FileInputStream(args[0]),charset) )
   String[] nl
+
+  // Cols: Postcode,Positional_quality_indicator,Eastings,Northings,Country_code,NHS_regional_HA_code,NHS_HA_code,Admin_county_code,Admin_district_code,Admin_ward_code
+
   while ((nl = r.readNext()) != null) {  
     log.debug("Procesing ${nl}");
     Double e = Double.valueOf(nl[2])
@@ -41,7 +50,8 @@ else {
     OSRef os1 = new OSRef(e.doubleValue(), n.doubleValue());
     LatLng ll1 = os1.toLatLng();
     ll1.toWGS84();
-    log.debug("Geocoded to lat:${ll1.getLat()} lon:${ll1.getLng()}");
+    log.debug("${nl[0]} Geocoded to lat:${ll1.getLat()} lon:${ll1.getLng()} country:${codes[nl[4]]} admin_county:${codes[nl[7]]} district:${codes[nl[8]]} ward:${codes[nl[9]]}");
+    writeGazRecord(esclient, "CodePointOpen",nl[0].trim(),"1.postcode",nl[0].trim(),nl[0].trim(), nl[0].trim(),null,ll1.getLat(),ll1.getLng())
   }
 }
 
@@ -60,6 +70,16 @@ else {
 //        addField(document, root, "lng",""+ll1.getLng());
 //      }
 
+def loadCodes() {
+  def result = [:]
+  CSVReader r = new CSVReader( new InputStreamReader(new FileInputStream('./refdata.csv')) )
+  String[] nl
+  while ( (nl = r.readNext()) != null) {
+    log.debug("Adding ${nl[1]} = ${nl[0]}");
+    result[nl[1]] = nl[0]
+  }
+  result
+}
 
 def inites() {
   println("Init");
@@ -132,3 +152,41 @@ def setup(esclient) {
 
     println("Installed mappings");
 }
+
+
+def writeGazRecord(esclient, p_authority_id,p_id,p_rtype,p_place_name,p_dispname, p_fqn,p_alias,p_centroid_lat,p_centroid_lon) {
+    def place_pojo = [
+          "id":"${p_id}".toString(),
+          "featureType":"${p_rectype}".toString(),
+          "authority":"${p_authority_id}".toString(),
+          "placeName":"${p_place_name}".toString(),
+          "alias":"${p_alias}".toString(),
+          "fqn":"${p_dispname}".toString(),
+          "timestamp":System.currentTimeMillis(),
+          // "prefixfqn":"${p_fqn}".toString(),
+          // "aliases":["${p_alias}".toString()],
+          location : [
+            "lat":p_centroid_lat,
+            "lon":p_centroid_lon
+          ]
+    ];
+
+    println("Indexing record of type ${rectype} with ID ${p_id}");
+
+    try {
+      def future = esclient.index {
+        index "gaz"
+        type "${rectype}"
+        // id id.toString()
+        source place_pojo
+      }
+      println("Indexed respidx:$future.response.index / resptp:$future.response.type / respid:$future.response.id")
+    }
+    catch ( Exception e ) {
+      println("Problem: ${e}");
+      e.printStackTrace();
+    }
+    finally {
+    }
+  }
+
